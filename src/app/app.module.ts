bcf0c495e5a820b152e7d84a7d9aa539c58117dc
bieniucieniu <mikibien@outlook.com>
2023-06-20 20:59:01 +0200

    add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [AppComponent, MainContentComponent, SearchBarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
