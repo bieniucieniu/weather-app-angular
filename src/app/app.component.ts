368e9d5128ed36b2a94a7a88052989d081b9eb36
bieniucieniu <mikibien@outlook.com>
2023-06-20 15:26:02 +0200

    base configuration for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styles: [],
+  template: `
+    <div>
+      <app-navbar></app-navbar>
+      <h1>{{ title }}</h1>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'weather-app-angular';
+  title = 'weather';
 }
