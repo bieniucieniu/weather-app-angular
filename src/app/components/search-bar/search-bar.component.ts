72e0960fa3859e15c2d7af698db09bdb57043571
bieniucieniu <mikibien@outlook.com>
2023-06-20 17:37:27 +0200

    add base searchbar comp and global css var

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  template: `
+    <p>
+      search-bar works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SearchBarComponent {
+
+}
