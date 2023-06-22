47e6e2935ac50cca1e18c19eb7dba49e50807896
bieniucieniu <mikibien@outlook.com>
2023-06-22 21:14:57 +0200

    add colecting data from service

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, Params } from '@angular/router';
+import {
+  WeatherService,
+  type Weather,
+  type WeatherProps,
+} from '@/app/services/weather.service';
 @Component({
   selector: 'app-forecast',
+  styles: [``],
   template: `
-    <p>
-      forecast works!
-    </p>
+    <div>
+      <button (click)="onClick()">log data</button>
+    </div>
   `,
-  styles: [
-  ]
 })
 export class ForecastComponent {
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private weather: WeatherService
+  ) {}
+  params: WeatherProps = {
+    latitude: 0,
+    longitude: 0,
+  };
+  weatherData: Weather = {};
 
+  ngOnInit() {
+    this.ActivatedRoute.queryParams.subscribe((params: Params) => {
+      this.params = {
+        latitude: Number(params['latitude']),
+        longitude: Number(params['longitude']),
+        forecastDays: params['forecastDays']
+          ? Number(params['forecastDays'])
+          : 7,
+      };
+    });
+    this.weather
+      .getWeather(this.params)
+      .subscribe((e) => (this.weatherData = e));
+  }
+  onClick() {
+    console.log(this.weatherData);
+  }
 }
