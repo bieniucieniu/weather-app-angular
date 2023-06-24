fc4ede0dcf9d2c643a0cafa3a31ed4ce8d0e5c63
bieniucieniu <mikibien@outlook.com>
2023-06-24 23:03:19 +0200

    add date pipe and chart js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, NgChartsModule],
+  providers: [DatePipe],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/forecast/forecast.module.ts
+++ src/app/components/forecast/forecast.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgChartsModule } from 'ng2-charts';
 
 import { ForecastRoutingModule } from './forecast-routing.module';
 import { ForecastComponent } from './forecast.component';
 
-
 @NgModule({
-  declarations: [
-    ForecastComponent
-  ],
-  imports: [
-    CommonModule,
-    ForecastRoutingModule
-  ]
+  declarations: [ForecastComponent],
+  imports: [NgChartsModule, CommonModule, ForecastRoutingModule],
 })
-export class ForecastModule { }
+export class ForecastModule {}
