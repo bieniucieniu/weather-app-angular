5f37bbfcaf5ac16c39eaaa54641faf9436e74cde
bieniucieniu <mikibien@outlook.com>
2023-06-27 16:13:39 +0200

    create base for favorite and last search info in localstorage

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,23 +1,46 @@
 import { Injectable } from '@angular/core';
 
+type placesInfo = {
+  name: string;
+  latitude: number;
+  longitude: number;
+};
 @Injectable({
   providedIn: 'root',
 })
 export class StorageService {
   constructor() {}
 
-  public save(key: string, value: string) {
-    localStorage.setItem(key, value);
+  favorite: placesInfo[] = [];
+  public addToFavorite(value: placesInfo) {
+    if (localStorage.getItem('favorite')) {
+      this.favorite = JSON.parse(localStorage.getItem('favorite') as string);
+      this.favorite.push(value);
+      localStorage.setItem('favorite', JSON.stringify(this.favorite));
+    }
   }
 
-  public get(key: string) {
-    return localStorage.getItem(key);
-  }
-  public remove(key: string) {
-    localStorage.removeItem(key);
+  public getFavorite() {
+    if (localStorage.getItem('favorite')) {
+      return JSON.parse(localStorage.getItem('favorite') as string);
+    }
   }
 
-  public clearAll() {
-    localStorage.clear();
+  public removeFromFavorite(value: placesInfo) {
+    if (localStorage.getItem('favorite')) {
+      this.favorite = JSON.parse(localStorage.getItem('favorite') as string);
+      this.favorite = this.favorite.filter((item) => item.name !== value.name);
+      localStorage.setItem('favorite', JSON.stringify(this.favorite));
+    }
+  }
+
+  public setLastSearch(value: placesInfo) {
+    localStorage.setItem('lastSearch', JSON.stringify(value));
+  }
+
+  public getLastSearch() {
+    if (localStorage.getItem('lastSearch')) {
+      return JSON.parse(localStorage.getItem('lastSearch') as string);
+    }
   }
 }
